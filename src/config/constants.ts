3f051803831edefb1920b00caa2e4b525e042f8b
princeessjay <vikaskashyaprock@gmail.com>
2024-12-20 13:00:16 +0530

    commit

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 // API endpoints and configuration
 export const API_CONFIG = {
   SOCKET_URL: import.meta.env.PROD 
-    ? 'https://rtc-l2n1.onrender.com/'
+    ? 'https://rtc-6xcw.onrender.com'
     : 'http://localhost:3000',
   RECONNECTION_ATTEMPTS: 5,
   RECONNECTION_DELAY: 1000,
