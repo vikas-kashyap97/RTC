0cf4af1a2ccf93759164b4408f1dc157020a05f0
princeessjay <vikaskashyaprock@gmail.com>
2024-12-20 15:12:20 +0530

    commit

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 // API endpoints and configuration
 export const API_CONFIG = {
   SOCKET_URL: import.meta.env.PROD 
-    ? 'https://rtc-6xcw.onrender.com'
+    ? 'https://api.render.com/deploy/srv-ctijbh5umphs73f3cs30?key=phCHpti_PGA'
     : 'http://localhost:3000',
   RECONNECTION_ATTEMPTS: 5,
   RECONNECTION_DELAY: 1000,
