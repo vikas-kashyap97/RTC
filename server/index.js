39efb18f5b297e4fe3e977331b29268cd81e27e0
princeessjay <vikaskashyaprock@gmail.com>
2024-12-20 13:49:36 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -58,6 +58,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+httpServer.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
